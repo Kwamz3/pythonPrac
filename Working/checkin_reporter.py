63c2a357cea434fddfca34fc137cf1a58db4a6e4
Kwamz3 <charlie18appiah@gmail.com>
2025-09-16 01:12:25 +0000

    Tue-09-25-1:12

--- Working/checkin_reporter.py
+++ Working/checkin_reporter.py
@@ -1,88 +1,107 @@
-import json
-from datetime import datetime
 import os
-BASE_FILE = "check_in.json"
-# --------------------- Helper functions -------------------
+from datetime import datetime
+import json
+
+BASE_FILE = 'check_ins.json'
+
+# ---------------- Helper Functions ----------------
+
 def load_list():
     """Loads check-in list"""
     if os.path.exists(BASE_FILE):
         with open(BASE_FILE, "r") as file:
             return json.load(file)
-    return []
+    return[]
+
 
 def save_list(checks):
-    """Saves current checks"""
+    """Save check-in list"""
     with open(BASE_FILE, "w") as file:
         json.dump(checks, file, indent=4)
- 
-    
-def checkin_list(checks):
-    """Add new check-ins"""
+        
+
+def add_list(checks):
+    """Add new check-in"""
     name = input("Enter your name: ").strip()
     if name != "":
         day_input = datetime.now().strftime("%d-%m-%y")
         time_input = datetime.now().strftime("%H:%M:%S")
-    
+        
     checkin_details = {
-        "name" : name,
-        "day" : day_input,
-        "time" : time_input
+        "name": name,
+        "day": day_input,
+        "time": time_input
     }
     
     checks.append(checkin_details)
     save_list(checks)
-    print("👍 Check-in success")
+    print("👍 Check-in successful!")
+    
     
 def view_list(checks):
-    """View check-in list"""
+    """View check-in lists"""
     if not checks:
         print("No check-ins made yet")
-        return
-    
+        
     for i, chk in enumerate(checks, 1):
         print(f"{i}.{chk['name']} | {chk['day']} | {chk['time']}")
         
         
 def filter_by_name_asc(checks):
-    """Filter by the names in ascending order"""
+    """Filter by name and display in ascending order"""
     filtered = sorted(checks, key=lambda chk: chk['name'].lower())
     for i, chk in enumerate(filtered, 1):
-        print(f"{i}.{chk['name']} | {chk['day']} | {chk['time']}")
+        print(f'{i}.{chk['name']} | {chk['day']} | {chk['time']}')
         
         
-def filter_by_time(checks):
-    """Filter by the time in ascending order"""
-    filtered = sorted(checks, key=lambda chk: (chk['day'], chk['time']))
+def filter_by_time_latest(checks):
+    """Filter by time and display from the latest"""
+    filtered = sorted(checks, key=lambda chk: chk(['day'], ['time']))
     for i, chk in enumerate(filtered, 1):
         print(f"{i}.{chk['name']} | {chk['day']} | {chk['time']}")
 
-# ------------------ Main Program ------------------------
+
+def clear_json(checks):
+    """Clear check-in Json"""
+    with open(BASE_FILE, "w") as file:
+        json.dump([], file, indent=4)
+        checks.clear()
+    print("Check-in data has been cleared.")
+        
+        
+        
+# ------------------- Main function -----------------
+
 def main():
+    
     check = load_list()
     
     while True:
-        print("\n === Check-in Tracker ===")
-        print("1. Check in")
-        print("2. View Checks")
-        print("3. Filter by name(Asc)")
+        print("\n === Check-in Monitor ===")
+        print("1. Add new Check-in")
+        print("2. View all Check-ins")
+        print("3. Filter by name(ASC)")
         print("4. Filter by time(latest)")
-        print("5. Exit")
+        print("5. Clear check-ins")
+        print("6. Goodbye")
         
-        choice = input("Enter your option: ").strip()
+        choice = input("Enter your choice: ").strip()
         
         if choice == "1":
-            checkin_list(check)
+            add_list(check)
         elif choice == "2":
             view_list(check)
         elif choice == "3":
             filter_by_name_asc(check)
         elif choice == "4":
-            filter_by_time(check)
+            filter_by_time_latest(check)
         elif choice == "5":
+            clear_json(check)
+        elif choice == "6":
             print("Goodbye...")
             break
         else:
-            print("Invalid choice... try again")
+            print("Invalid choice! try again...")
             
 if __name__ == "__main__":
     main()
