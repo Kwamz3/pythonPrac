15f6562c7926289c84a1e8057c99ab4c523f8add
Kwamz3 <charlie18appiah@gmail.com>
2026-01-31 13:00:51 +0000

    Add examples of nested conditionals in L4_conditionals.py

--- school/L4_conditionals.py
+++ school/L4_conditionals.py
@@ -34,3 +34,21 @@
 #     print('...')
 #     print('Done.')
 # print('After conditional')
+
+
+
+# Does line execute?                        Yes    No
+#                                           ---    --
+if 'foo' in ['foo', 'bar', 'baz']:        #  x
+    print('Outer condition is true')      #  x
+
+    if 10 > 20:                           #  x
+        print('Inner condition 1')        #        x
+
+    print('Between inner conditions')     #  x
+
+    if 10 < 20:                           #  x
+        print('Inner condition 2')        #  x
+
+    print('End of outer condition')       #  x
+print('After outer condition')            #  x
