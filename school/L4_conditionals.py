ab01119dbe97e9acfaf49450549477b4abd9cd9e
Kwamz3 <charlie18appiah@gmail.com>
2026-01-31 13:30:52 +0000

    Enhance L4_conditionals.py with additional examples of nested conditionals, else and elif usage, and dictionary lookups for conditionals

--- school/L4_conditionals.py
+++ school/L4_conditionals.py
@@ -36,19 +36,63 @@
 # print('After conditional')
 
 
+# # a much more complicated script to prove the importance of indentation in conditionals
+# # Does line execute?                        Yes    No
+# #                                           ---    --
+# if 'foo' in ['foo', 'bar', 'baz']:        #  x
+#     print('Outer condition is true')      #  x
 
-# Does line execute?                        Yes    No
-#                                           ---    --
-if 'foo' in ['foo', 'bar', 'baz']:        #  x
-    print('Outer condition is true')      #  x
+#     if 10 > 20:                           #  x
+#         print('Inner condition 1')        #        x
 
-    if 10 > 20:                           #  x
-        print('Inner condition 1')        #        x
+#     print('Between inner conditions')     #  x
 
-    print('Between inner conditions')     #  x
+#     if 10 < 20:                           #  x
+#         print('Inner condition 2')        #  x
 
-    if 10 < 20:                           #  x
-        print('Inner condition 2')        #  x
+#     print('End of outer condition')       #  x
+# print('After outer condition')            #  x
 
-    print('End of outer condition')       #  x
-print('After outer condition')            #  x
+
+# # adding the else keyword to determine an alternative path
+# x = 20
+
+# if x < 50:
+#     print('(first suite)')
+#     print('x is small')
+# else:
+#     print('(second suite)')
+#     print('x is large')
+    
+
+# # the use of elif keyword to add more alternatives to the conditional
+# name = 'Joe'
+# if name == 'Fred':
+#     print('Hello Fred')
+# elif name == 'Xander':
+#     print('Hello Xander')
+# elif name == 'Joe':
+#     print('Hello Joe')
+# elif name == 'Arnold':
+#     print('Hello Arnold')
+# else:
+#     print("I don't know who you are!")
+
+
+# # We can use the dict.get() method to search for a word and pass a default value if it isn't found
+# names = {
+#     'Fred': 'Hello Fred',
+#     'Xander': 'Hello Xander',
+#     'Joe': 'Hello Joe',
+#     'Arnold': 'Hello Arnold'
+# }
+
+# print(names.get('Joe', "I don't know who you are!"))
+# print(names.get('Rick', "I don't know who you are!"))
+
+
+# # you can write all the expressions of a condtional on the same line seperated by semi-colons
+# if 'f' in 'foo': print('1'); print('2'); print('3')
+
+
+# 
