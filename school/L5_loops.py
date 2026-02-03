bae66856286b3eba1e2ecb13658f5af3d176cef7
Kwamz3 <charlie18appiah@gmail.com>
2026-02-03 11:52:16 +0000

    Improve comments in L5_loops.py for clarity and completeness

--- school/L5_loops.py
+++ school/L5_loops.py
@@ -1,4 +1,13 @@
-# while loop works by evaluating a condition at the start of a condition
-# they're also useful for for loops in which the number of iterations are unknown
+# while loop works by evaluating a condition at the start of a condition.
+
+# they're also useful for for loops in which the number of iterations are unknown.
+
 # a while True loop creates an infinte loop that continues to execute until the a break statement is introduced-
-# or an external interuption occurs
+# or an external interuption occurs.
+
+# loops are control flow statements that excute a set of code a number of times 
+
+# a for loop is used for a known number of times 
+# a while loop is used for a unknown number of times
+
+ 
