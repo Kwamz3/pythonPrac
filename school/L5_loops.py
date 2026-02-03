5afb3b7116816936dd684ce3e0cc27a977cbb40d
Kwamz3 <charlie18appiah@gmail.com>
2026-02-03 11:47:33 +0000

    Add initial comments explaining while loops in L5_loops.py

---
+++ school/L5_loops.py
@@ -0,0 +1,4 @@
+# while loop works by evaluating a condition at the start of a condition
+# they're also useful for for loops in which the number of iterations are unknown
+# a while True loop creates an infinte loop that continues to execute until the a break statement is introduced-
+# or an external interuption occurs
